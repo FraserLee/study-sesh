ad529e369ef1eee1e1374d01cd83ad99f52ee8ba
Fraser <tomatoesneverlie@gmail.com>
2022-11-03 23:06:39 -0400

    refinements to post system
    both styling and slight functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,23 +16,47 @@ const Home: NextPage = () => {
         <Auth />
       </div>
 
-      <main className="mx-auto min-h-screen flex flex-col justify-center items-center p-4 gap-2 w-fit">
-        <h1 className="text-8xl text-white bg-[#6636305e] px-5 text-center">
+      <main className="mx-auto min-h-screen flex flex-col items-center p-4 gap-2 w-fit text-white">
+        <h1 className="text-8xl text-white bg-[#6636305e] px-5 text-center mt-[calc(50vh-7.5rem)]">
           study-sesh.app
         </h1>
-        <div className="flex flex-col gap-2 items-start w-full px-5">
-          <p className="text-lg text-white">
-            Study together in person with other McGill students.<br/>
+        <div className="flex flex-col gap-2 items-start w-full px-5 text-lg">
+          <p>
+            Study together in person with other McGill students.
+          </p>
+          <p>
+            We haven't built it yet, so for the time being have fun with this
+            textbox.
           </p>
         </div>
-        <PostMessageBox />
-        <Posts />
+        <div className="flex flex-col gap-2 items-end w-full px-5 mt-10">
+          <PostMessageBox />
+          <Posts/>
+        </div>
       </main>
     </>
   );
 };
 
 export default Home;
+
+
+
+
+
+
+const Button = (props: { 
+                onClick?: () => void, 
+                type?: "button" | "submit" | "reset", 
+                children: React.ReactNode
+}) => (
+  <button
+    className="text-white font-mono text-xs bg-[#6636305e] px-2 py-1 hover:bg-black hover:text-white"
+    type={props.type ?? "button"}
+    onClick={props.onClick}
+  >{props.children}</button>
+);
+
 
 const Auth: React.FC = () => {
   const { data: sessionData } = useSession();
@@ -44,12 +68,9 @@ const Auth: React.FC = () => {
           logged in as {sessionData?.user?.name}
         </p>
       )}
-      <button
-        className="text-white font-mono text-xs bg-[#6636305e] px-2 py-1 hover:bg-black hover:text-white"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
+      <Button onClick={sessionData ? () => signOut() : () => signIn()}>
         {sessionData ? "sign out" : "sign in"}
-      </button>
+      </Button>
     </div>
   );
 };
@@ -62,8 +83,20 @@ const PostMessageBox = () => {
   const [message, setMessage] = useState("");
   const post = trpc.post.post.useMutation();
 
+  if (!sessionData) {
+    return (<>
+      <p className="text-white px-2 py-1 text-right text-sm opacity-50">
+        howdy, stranger<br/>
+        you've gotta sign in to post round these parts<br/>
+        -🤠
+      </p>
+      <Button onClick={() => signIn()}>sign in</Button>
+    </>);
+  }
+
   return (
     <form
+      className="flex flex-col gap-2 items-end w-full"
       onSubmit={(e) => {
         e.preventDefault();
         post.mutate({
@@ -74,20 +107,16 @@ const PostMessageBox = () => {
       }}
     >
       <input
-        className="w-full px-2 py-1"
+        className="px-2 py-1 text-white bg-[#6636305e] text-sm outline-none w-96"
         value={message}
+        placeholder="what's up?"
         minLength={1}
         maxLength={100}
         onChange={(e) => setMessage(e.target.value)}
       />
-      <button className="w-full px-2 py-1" type="submit">
-        Post
-      </button>
+      <Button type="submit">post</Button>
     </form>
   );
-
-
-
 }
 
 
@@ -101,7 +130,7 @@ const Posts = () => {
     <div className="flex flex-col gap-4 text-white">
       {messages?.map((msg, index) => {
         return (
-          <div key={index}>
+          <div key={index} className="bg-[#66363030] px-2 py-1 font-mono text-xs w-80">
             <p>{msg.message}</p>
             <span>- {msg.name}</span>
           </div>
