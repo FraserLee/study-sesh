760de97a615d27317e33200adc26cef0ea8cb596
Fraser <tomatoesneverlie@gmail.com>
2022-11-11 19:57:11 -0500

    editable name
    Frontend logic working, but I still need to write the api endpoint to update the db.

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,55 +1,52 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
-import { signIn, signOut, useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 import Button from "../components/button";
-
 import Avatar from "boring-avatars";
 
 
 
 
-/*
 const EditableField = ({ value, onChange }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [inputValue, setInputValue] = useState(value);
 
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
+  const edit = () => { setIsEditing(true); };
 
-  const handleCancel = () => {
+  const cancel = () => {
     setIsEditing(false);
     setInputValue(value);
   };
 
-  const handleSave = () => {
+  const save = () => {
     setIsEditing(false);
     onChange(inputValue);
-  };
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
   };
 
   return (
     <div className="flex flex-col">
       {isEditing ? (
         <div className="flex flex-row">
-          <input className="text-black" value={inputValue} onChange={handleInputChange} />
-          <button onClick={handleSave}>Save</button>
-          <button onClick={handleCancel}>Cancel</button>
+          <input 
+            className="px-2 py-1 text-white bg-[#6636305e] text-sm outline-none"
+            value={inputValue} 
+            onChange={
+              (e: React.ChangeEvent<HTMLInputElement>) => { 
+                setInputValue(e.target.value); 
+          } } />
+          <Button onClick={cancel}>cancel</Button>
+          <Button onClick={save}>save</Button>
         </div>
       ) : (
-        <div className="flex flex-row">
+        <div className="flex flex-row place-content-between">
           <span>{value}</span>
-          <button onClick={handleEdit}>Edit</button>
+          <Button onClick={edit}>edit</Button>
         </div>
       )}
     </div>
   );
 };
-*/
 
 
 // (property) Session.user?: ({
@@ -61,9 +58,8 @@ const EditableField = ({ value, onChange }) => {
 // }) | undefined
 
 const Profile = ({ user } : { user: any }) => {
-  const name = user?.name || '';
+  const [name, setName] = useState(user?.name || '');
   const email = user?.email || '';
-
   const image = user?.image ? (
     <img className="rounded-full w-24 h-24" src={user?.image} />
   ) : (
@@ -75,24 +71,21 @@ const Profile = ({ user } : { user: any }) => {
     />
   );
 
+  const updateName = (newName: string) => {
+    setName(newName);
+    alert("todo: update name in db");
+  };
+
   // display a profile section for the user, with a rounded image, name, and email
   return (
-    <div className="flex flex-row items-start gap-2">
+    <div className="flex flex-row items-start gap-2 w-full">
         {image}
-        <div className="flex flex-col my-2">
-          <p className="font-bold">{name}</p>
-          <p className="text-sm">{email}</p>
+        <div className="flex flex-col my-2 w-full">
+          <EditableField value={name} onChange={updateName} />
+          <p className="text-xs">{email}</p>
         </div>
     </div>
   );
-
-      
-      
-
-
-
-
-
 }
 
 const ProfilePage: NextPage = () => {
@@ -116,8 +109,8 @@ const ProfilePage: NextPage = () => {
       <Head>
         <title>Profile</title>
       </Head>
-      <main className="mx-auto min-h-screen flex flex-col items-left p-4 gap-2 w-fit ">
-        <h1 className="text-6xl bg-[#6636305e] px-5 text-center mt-[calc(20vh)]">
+      <main className="mx-auto min-h-screen flex flex-col items-left p-4 gap-2 w-[24rem] ">
+        <h1 className="text-6xl bg-[#6636305e] px-5 text-center mt-[calc(20vh)] w-full">
           Profile
         </h1>
         {content}
