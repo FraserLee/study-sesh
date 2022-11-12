ea872a8a00971df4a0dcedc1d2301feb6da7115f
Fraser <tomatoesneverlie@gmail.com>
2022-11-11 19:34:40 -0500

    fixed wrapping login.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,25 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Button from "../components/button";
 
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
-  Component,
-  pageProps: { session, ...pageProps },
-}) => {
-  return (
-    <SessionProvider session={session}>
 
-      {/* Sign-in button fixed in the top left corner */}
-
+// I've separated MyApp into two components, since I don't know any other
+// way to access the status of the session. Inner one wraps it in a login
+// button, or a name if logged in.
+// 
+// This is janky. There's definitely a better way to do this, if anyone knows
+// enough react to work it out.
+const InnerApp = ({ Component, pageProps }: any) => {
+  const { data: session, status } = useSession();
+  return (<>
+    {/* Sign-in button fixed in the top left corner */}
+    {status !== "loading" && (
       <div className="fixed top-0 left-0 z-50 m-2">
         <div className="flex flex-col items-start gap-2">
           {session && (
@@ -32,10 +35,21 @@ const MyApp: AppType<{ session: Session | null }> = ({
           </Button>
         </div>
       </div>
+    )}
 
-      {/* Page content */}
-      <Component {...pageProps} />
+    {/* Page content */}
+    <Component {...pageProps} />
+  </>);
+};
 
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <InnerApp Component={Component} pageProps={pageProps} />
     </SessionProvider>
   );
 };
