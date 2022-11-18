3f2d43a941604b52c9ed6bc8381d10185505f718
Fraser <tomatoesneverlie@gmail.com>
2022-11-18 17:52:31 -0500

    mostly non-functional
    I'm going to abandon this for now, seems hard. I was just trying to create a quick dev account so when running locally you can test out stuff.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,6 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
+import CredentialsProvider from "next-auth/providers/credentials";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
@@ -19,10 +20,43 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
+
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+
+    CredentialsProvider({
+      name: "dev-mode credentials",
+      credentials: {
+        name: {
+          label: "Name",
+          type: "text",
+          placeholder: "Enter your name",
+        },
+      },
+      async authorize(credentials) {
+        const id = Math.random().toString(36).slice(2);
+        const name = credentials?.name || "Dev Mode User";
+        const email = name + " < dev mode >";
+        const session = await prisma.session.create({
+          data: {
+            id,
+            userId: id,
+            sessionToken: id,
+            expires: new Date(Date.now() + 60 * 60 * 1000),
+          }
+        });
+        const user = await prisma.user.upsert({
+          where: { email },
+          update: {},
+          create: { id, name, email, sessions: { connect: { id: session.id } } },
+        });
+
+        return user;
+      },
+    }),
+
     // ...add more providers here
   ],
 };
